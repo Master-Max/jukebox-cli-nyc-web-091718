c4cafed8b9311965ab4f7f8b4416904ea3ba5967
Max Petersen <mglxp5@gmail.com>
2018-09-18 13:49:43 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,54 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+songs_hash = {
+  "Phoenix" => "1901",
+  "Tokyo Police Club" => "Wait Up",
+  "Sufjan Stevens" => "Too Much",
+  "The Naked and the Famous" => "Young Blood",
+  "(Far From) Home" => "Tiga",
+  "The Cults" => "Abducted",
+  "Phoenix" => "Consolation Prizes",
+  "Harry Chapin" => "Cats in the Cradle",
+  "Amos Lee" => "Keep It Loose, Keep It Tight"
+}
+
+#def songs_hash
+  #songs_hash = {}
+  #songs.each do |song|
+    #split_song = song.split
+    #songs_hash[split_song[0]] = split_song[1]
+  #end
+  #songs_hash
+#end
+
+def say_hello(name)
+  "Hi #{name}!"
+end
+
+def list
+  i = 0
+  while i < songs.size do
+    puts "#{i+1}. #{songs[i]}"
+    i += 1
+end
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of song you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+  puts "Please enter a command:"
+end
+
+def play
+  puts "Please enter a song name or number:"
+  entry = gets.chomp
+  
+end
+
+#puts "Enter your name:"
+#users_name = gets.chomp
+
+#puts say_hello(users_name)
